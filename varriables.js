5360620b399e820d61fb2a90df885d68098eefb9
Ali Ahmad <aliahmad28042003@gmail.com>
2025-09-25 22:55:45 +0500

    Difference between var, const, let - block scope

---
+++ varriables.js
@@ -0,0 +1,25 @@
+const account_Id=144533                                 
+
+let account_Email="aliahmad@gmail.com"
+
+var account_Password="pass@123"
+
+account_City="Islamabad"
+
+let account_state
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// account_Id=111                           // not allowed
+
+account_Email="ali@gmail.com"
+
+account_Password="pass"
+
+account_City="NewYork"
+
+
+
+console.table([account_Id,account_Email,account_Password,account_City,account_state])
+
+
