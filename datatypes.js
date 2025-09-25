3e87e9129b94a8e8cfcd712b6ac6c44d8d4c9682
Ali Ahmad <aliahmad28042003@gmail.com>
2025-09-25 23:36:22 +0500

    intro to datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict"
+
+let Name = "Ali Ahmad"
+
+let age = 22
+
+let income = "200K"
+
+let isWorking = true
+
+
+console.table([Name,age,income,isWorking])
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof income);
+console.log(typeof isWorking);
+
+
+console.log(typeof null);
+
+console.log(typeof undefined);
