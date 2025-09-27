7683177c39e1a2dfacf203eef254ec7a88bdeab3
Ali Ahmad <aliahmad28042003@gmail.com>
2025-09-27 23:00:45 +0500

    added the Js file of simple datatype conversions

---
+++ conversion_Operation.js
@@ -0,0 +1,43 @@
+
+let score = "33"
+
+let valueInNumber=Number(score)
+
+// console.log(typeof(score))
+// console.log(typeof(valueInNumber))
+
+
+// let newnumber="33abc"
+
+// let newNumberInNumber=Number(newnumber)
+
+// console.log(newNumberInNumber)
+// console.log(typeof newNumberInNumber)
+
+// let test = "Ali"
+// console.log(typeof (test))
+// console.log(test)
+
+// let test_number=Number(test)
+// console.log(typeof (test_number))
+// console.log(test_number)
+
+
+// let isLoggedIn=1
+// console.log("Before conversion !! \n ")
+// console.log(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log("After conversion to the boolean !!!! \n ")
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+
+
+let stringNumber=996654
+console.log(stringNumber)
+console.log(typeof stringNumber,"\n")
+
+let toNumber=String(stringNumber)
+console.log(toNumber)
+console.log(typeof toNumber,"\n")
